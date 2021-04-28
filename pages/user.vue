0ed9633191ef96fb45cd70e5d50016fc2a8d88ae
ch634421335 <ch634421335@gmail.com>
2021-04-28 09:46:16 +0800

    修改端口号3001

--- pages/user.vue
+++ pages/user.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="user">
     <h3>user</h3>
+    <el-input
+      class="pro_input"
+      v-model="formData.astrict"
+      size="mini"
+      style="width:100px"
+      @keyup.native="proving1"
+    ></el-input>
   </div>
 </template>
 <script>
@@ -9,5 +16,25 @@ export default {
   //   console.log("middleware pages 组件独享守卫");
   //   redirect("/login");
   // }
+  data() {
+    return {
+      formData: { astrict: "" }
+    };
+  },
+  methods: {
+    proving1() {
+      this.formData.astrict = this.formData.astrict.replace(/[^\.\d]/g, ""); //清除"数字"和"."以外的字符
+      this.formData.astrict = this.formData.astrict.replace(/^\./g, ""); //验证第一个字符是数字而不是.
+      this.formData.astrict = this.formData.astrict.replace(/\.{2,}/g, "."); //只保留第一个. 清除多余的.
+      this.formData.astrict = this.formData.astrict
+        .replace(".", "$#$")
+        .replace(/\./g, "")
+        .replace("$#$", "."); //只允许输入一个小数点
+      this.formData.astrict = this.formData.astrict.replace(
+        /^(\-)*(\d+)\.(\d\d).*$/,
+        "$1$2.$3"
+      ); //只能输入两个小数
+    }
+  }
 };
 </script>
--- plugins/mixin.js
+++ plugins/mixin.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 let show = () => console.log("全局方法");
-Vue.prototype.$show = show; //服务端的狗子内部无法使用,this不会指向vue实例
+Vue.prototype.$show = show; //服务端的钩子内部无法使用,this不会指向vue实例
 
 import * as filters from "../assets/script/filters";
 
